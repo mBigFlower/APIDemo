90891903cc098ae527e82b5ace94d7952694ce44
mBigflower <heilongjiang333@126.com>
2018-11-08 08:47:13 +0800

    detail style

--- server/app.js
+++ server/app.js
@@ -13,6 +13,8 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../bs'))); // __dirname为硬盘的目录，而非网址的目录
  
 app.get('/', function (req, res) {
+   var remoteIp = req.connection.remoteAddress;
+   console.log('remoteIp:'+remoteIp);
    var filePath = path.join(__dirname, '../bs/html/home.html');
    res.sendFile(filePath);
 })
@@ -63,8 +65,8 @@ app.post('/login_post', urlencodedParser, function (req, res) {
    res.end(JSON.stringify(response));
 })
 
-
-var server = app.listen(8081, function () {
+// listen 的时候,加上 0.0.0.0 这样 remoteAddress 收到的是 IPv4
+var server = app.listen(8081, '0.0.0.0', function () {
  
   var host = server.address().address
   var port = server.address().port
