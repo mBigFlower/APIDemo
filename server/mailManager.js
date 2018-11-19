924313e5d8a4da9889c8815941b447281904bbff
bigflower <heilongjiang333@126.com>
2018-11-19 23:36:12 +0800

    make the email ok
    Host rather than Server

--- server/mailManager.js
+++ server/mailManager.js
@@ -1,8 +1,9 @@
 var nodemailer  = require('nodemailer');
 
 var mailTransport = nodemailer.createTransport({
-    service : 'smtp.126.com',
+    host : 'smtp.126.com',  // 妈蛋，这里是host，而不是server。坑惨了
     secureConnection: true, // 使用SSL方式（安全方式，防止被窃取信息）
+    port: 465, // SMTP 端口
     auth : {
         user : 'heilongjiang333@126.com',
         pass : 'aijiang2shi'
@@ -12,7 +13,7 @@ var mailTransport = nodemailer.createTransport({
 function SendEmail(){
 	var options = {
         from        : 'heilongjiang333@126.com',
-        to          : 's_h_chen@126.com',
+        to          : '252565596@qq.com',
         // cc         : ''  //抄送
         // bcc      : ''    //密送
         subject        : '一封来自126网易333的邮件',
